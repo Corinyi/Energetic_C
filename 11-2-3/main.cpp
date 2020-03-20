fab8be9b9fee51d32cb4e3705f68770b7fbbac56
Corinyi <leejimmy0407@gmail.com>
2020-03-20 23:19:31 +0900

    11-2-3 added

---
+++ 11-2-3/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  11-2-3
+//
+//  Created by Corinyi on 2020/03/20.
+//  Copyright © 2020 Corinyi. All rights reserved.
+//
+
+# include <stdio.h>
+
+int main(void){
+    char voca[30];
+    int len=0;
+    int max=0;
+    
+    printf("영단어를 입려하세요: ");
+    scanf("%s", voca);
+    
+    while(voca[len] != '\0'){
+        len++;
+    }
+    
+    for(int i=0;i<len;i++){
+        max = (max > voca[i]) ? max : voca[i];
+    }
+    
+    
+    
+    
+    printf("아스키 코드가 가장 큰 문자는: %c \n", max);
+    
+    return 0;
+}
