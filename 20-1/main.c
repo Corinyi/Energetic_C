0d590bb5cd57b6866909c627f840f3741ff72186
Corinyi <leejimmy0407@gmail.com>
2020-07-02 18:18:22 +0900

    add Challenge Programming 3-1(20-1)

---
+++ 20-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  20-1
+//
+//  Created by 이윤서 on 2020/07/02.
+//  Copyright © 2020 Corinyi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
